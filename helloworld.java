d91bfeb145f4437e1fcad0918ce856cac3f3ca39
Sam Shiao <srshiao@gmail.com>
2018-01-06 11:34:41 -0500

    Fixed the syntax of minus method
    Also added a print statement for minus to check that it works

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,11 @@
 public static void main(String[] args){
     System.out.println("Hello, world!"); 
+	//added this line
+	System.out.println(minus(5,1));
 }
 
 public int minus(int a, int b){
-    return a - b;
+    return (a-b);
 }
 
 public int add(int a, int b) {
